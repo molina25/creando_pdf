784b1f26c37d8171d1ab48de843c4bfd795a02e0
molina25 <molinadennis246@gmail.com>
2020-06-22 15:41:05 -0600

    agregando libreria

--- src/java/codigo/crearpdf.java
+++ src/java/codigo/crearpdf.java
@@ -4,11 +4,30 @@
  * and open the template in the editor.
  */
 package codigo;
+import com.itextpdf.text.BaseColor;
+import com.itextpdf.text.Chunk;
+import com.itextpdf.text.Document;
+import com.itextpdf.text.Element;
+import com.itextpdf.text.Font;
+import com.itextpdf.text.FontFactory;
+import com.itextpdf.text.Image;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.Phrase;
+import com.itextpdf.text.pdf.PdfPCell;
+import com.itextpdf.text.pdf.PdfPTable;
+import com.itextpdf.text.pdf.PdfWriter;
+import com.mysql.jdbc.Connection;
+import com.mysql.jdbc.Statement;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
-/**
- *
- * @author user
- */
 public class crearpdf {
     
 }
